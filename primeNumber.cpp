b23cbba2720d6e1e982b1e2c3fd136301b2630fc
iamshiv007 <iamshiv20032003@gmail.com>
2023-08-28 14:11:42 +0530

    check prime or not

---
+++ primeNumber.cpp
@@ -0,0 +1,28 @@
+// check prime or not
+#include <iostream>
+
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    std::cout << "Enter a positive integer: ";
+    std::cin >> num;
+
+    if (isPrime(num)) {
+        std::cout << num << " is a prime number." << std::endl;
+    } else {
+        std::cout << num << " is not a prime number." << std::endl;
+    }
+
+    return 0;
+}
